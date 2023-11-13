6271b7e1581b0c5c2e06c8883f6673d229cda142
dashuta <it.dashutka@gmail.com>
2023-11-13 23:45:13 +0300

    fix views.py, delete 19_1.html

--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 
 
 def contact_info(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
     return render(request, 'catalog/contact_info.html')
 
 
